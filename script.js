20de0422feac21647341462ee886505ac8bb0d2c
farahmadkour816 <154679438+farahmadkour816@users.noreply.github.com>
2023-12-25 00:51:41 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleInformation(containerId) {
+    var container = document.getElementById(containerId);
+
+    // Toggle the "show-information" class on the specific container
+    container.classList.toggle("show-information");
+}
+
+
+
+
